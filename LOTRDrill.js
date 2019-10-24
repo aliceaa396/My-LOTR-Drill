ae20ae28f69e6b12193e4fb112bbe9047ed12551
Angel Alicea <ala31996@gmail.com>
2019-10-23 23:32:25 -0400

    tried to solve the drill to no avail

--- LOTRDrill.js
+++ LOTRDrill.js
@@ -1,26 +1,33 @@
-const createCharacter = {
+function createCharacter ( name, nickname, race, origin, attack,defense) {
+  let newObj = {
     name,
     nickname,
     race,
     origin,
     attack,
     defense,
-    describe:function(){
+    describe:function () {
       return `${name} is a ${race} from ${origin}.`;
     }
-    evaluateFight:function(character){
+    /*evaluateFight:function (character) {
       let oppDam = character.attack - character.defense;
       let yourDam = character.attack - character.defense;
-    }
-    return `Your opponent takes ${oppDam} damage and you take ${youDam} damage.`
+    */
   };
-  
+  //return `Your opponent takes ${oppDam} damage and you take ${youDam} damage.`
+}
+console.log (`Your opponent takes ${oppDam} damage and you take ${youDam} damage.`)
   let character = [
-    createCharacter.forEach(describe.evaluateFight)
+    const createCharacter.gandalf = ['Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6]
+    const createCharacter.bilbo = ['Bilbo Baggins','bilbo','Hobbit' , 'The Shire' , 2 , 1]
+    const createCharacter.frodo = ['Frodo Baggins','frodo','Hobbit','The Shire' 3, 2]
+    const createCharacter.aragorn = ['Aragorn son of Arathorn' , 'aragorn' , 'Man' , 'Dunnedain' , 6, 8]
+    const createCharacter.legolas = ['Legolas', 'legolas' , 'Elf' ,'Woodland Realm', 8 , 5]
+    character.forEach(describe.evaluateFight);
   ]
   
-  character.push('Angel, Anhell, Wolf,Parts Uknown,7,8');
+  character.push('Angel, Legna, Wolf,Parts Uknown,7,8');
   
-  arr.find(arragon(describe));
+  character.find(arragon(describe));
   
-  arr.filter(characters.attack > 5);
+  character.filter(Createcharacters.attack > 5;
